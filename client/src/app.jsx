d5eb16e8b356b7d86e55b37cef074daf3d3be8da
DavidRez <davidraviles@gmail.com>
2018-10-30 15:14:49 -0700

    App renders CustomerReview component for one product.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CustomerReview from './components/CustomerReview.jsx';
 
 class App extends React.Component {
     render() {
-        return (<div></div>)
+        return (<div><CustomerReview productId={1} /></div>)
     }
 }
 
