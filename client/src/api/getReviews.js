1d1cdf6bca233a401289bbfc493689ac8d15f3e9
DavidRez <davidraviles@gmail.com>
2018-11-06 14:58:31 -0800

    Cleaned up the code.

--- client/src/api/getReviews.js
+++ client/src/api/getReviews.js
@@ -1,5 +1,4 @@
 const getReviews = (productId) => {
-    console.log('original getreviews!');
     return fetch('http://localhost:5000/api/reviews/' + productId)
         .then(response => {
             return response.json();
