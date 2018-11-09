88401501885ada66b03333c3a755df14d560f960
DavidRez <davidraviles@gmail.com>
2018-11-08 19:08:03 -0800

    Made seed changes due to switch to ElephantSQL.

--- database/seeds/development/rainforest.js
+++ database/seeds/development/rainforest.js
@@ -2,23 +2,12 @@ const customerReviewData = require('../../customer_review.js');
 const productInfoData = require('../../product_info.js');
 const customerReviewImagesData = require('../../customer_review_images.js');
 
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('customer_review_images').del()
+  return knex('product_info').insert(productInfoData)
   .then(() => {
-    return knex('customer_review').del();
+    return knex('customer_review').insert(customerReviewData);
   })
   .then(() => {
-    return knex('product_info').del();
-  })
-  .then(() => {
-    return knex('product_info').insert(productInfoData);
-  })
-  .then(() => {
-    setTimeout(() => {return knex('customer_review').insert(customerReviewData);}, 500);
-  })
-  .then(() => {
-    setTimeout(() => {return knex('customer_review_images').insert(customerReviewImagesData);}, 500);
+    return knex('customer_review_images').insert(customerReviewImagesData);
   })
 };
