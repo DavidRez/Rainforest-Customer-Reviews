96fa67ad2d6026bf45de7a4c7da0515723934c33
DavidRez <davidraviles@gmail.com>
2018-11-09 12:26:18 -0800

    Added star rating widget and button to increase helpful count.

--- client/src/components/CustomerReview.jsx
+++ client/src/components/CustomerReview.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import moment from 'moment';
 import ReviewImages from './ReviewImages.jsx';
+import StarRatings from 'react-star-ratings';
+
 
 class CustomerReview extends React.Component {
     constructor(props) {
         super(props);
+
+        this.helpfulCount = this.helpfulCount.bind(this);
+
         this.state = {
+            count : false,
             info : {}
         };
     }
 
+    helpfulCount(e) {
+        if (!this.state.count) {
+            this.setState({count : true});
+            return fetch('/api/reviews/' + this.state.info.id, {
+                method: 'PATCH'})
+            .then(response => {
+                return response.json();
+            });
+        }
+    }
+    
     componentDidMount() {
         this.setState({info : this.props.info});
     }
 
     render() {
-        if(this.state.info !== undefined) {
+        if(this.props.info !== undefined) {
             return (
             <div className="review">
                 <div className="user">
@@ -23,17 +40,26 @@ class CustomerReview extends React.Component {
                     <div className="username">{this.state.info.customer_username}</div>
                 </div>
                 <div className="rating_title_box">
-                    <div className="stars">{this.state.info.rating} out of 5 stars | </div>
+                    <div className="stars">
+                        <StarRatings
+                            rating={this.state.info.rating}
+                            starRatedColor="rgb(255, 213, 79)"
+                            numberOfStars={5}
+                            name='rating'
+                            starDimension="16px"
+                            starSpacing="2px"
+                        />
+                    </div>
                     <div className="title">{this.state.info.title}</div>
                 </div>
                 <div className="date">{moment(this.state.info.review_date).format('ll')}</div>
                 <div className="version">version: {this.state.info.product_version}</div>
                 <div className="text">{this.state.info.body}</div>
-                <ReviewImages reviewId={this.state.info.id} />
+                <ReviewImages reviewId={this.props.info.id} />
                 <div className="bottom_box">
                     <div className="helpful">{this.state.info.helpful_count} people found this helpful</div>
-                    <button className="helpful-button">Helpful</button>
-                    <div className="buttons">| Comment | Report abuse</div>
+                    <button className="helpful-button" onClick={this.helpfulCount}>Helpful</button>
+                    <div className="buttons">| <a href="#">Comment</a> | <a href="#">Report abuse</a></div>
                 </div>
             </div>
             )
