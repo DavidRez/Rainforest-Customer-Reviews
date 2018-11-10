38501989494216651496d6d1f0c97025d938867f
DavidRez <davidraviles@gmail.com>
2018-11-10 12:26:27 -0800

    Proxy integrates all four services.

--- server/proxy.js
+++ server/proxy.js
@@ -1,11 +1,61 @@
-var http = require('http');
 var httpProxy = require('http-proxy');
-
+const express = require('express');
+let app = express();
 var proxy = httpProxy.createProxyServer({});
+let customerReview = 'http://localhost:2000';
+let productView = 'http://localhost:710';
+let recommendedProducts = 'http://localhost:3001';
+let productAttribute = 'http://localhost:8000';
 
-var server = http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'http://127.0.0.1:5000' });
+app.all("/productsdisplay", function(req, res) {
+    proxy.web(req, res, {target: productView});
+});
+
+app.all("/products", function(req, res) {
+    proxy.web(req, res, {target: recommendedProducts});
+});
+
+app.all("/api/productdata/product", function(req, res) {
+    proxy.web(req, res, {target: productAttribute});
+});
+
+app.all('/', (req, res) => {
+    proxy.web(req, res, {
+        target: customerReview
+      })
+});
+
+app.get('/cr/reviews', (req, res) => {
+    proxy.web(req, res, {
+        target: customerReview
+      })
+});
+
+app.get('/cr/reviews/:productid', (req, res) => {
+    let productid = req.params.productid;
+    proxy.web(req, res, {
+        target: customerReview
+      })
+});
+
+app.patch('/cr/reviews/:reviewId', (req, res) => {
+    let reviewId = req.params.reviewId;
+    proxy.web(req, res, {
+        target: customerReview
+      })
+});
+
+app.get('/cr/images', (req, res) => {
+    proxy.web(req, res, {
+        target: customerReview
+      })
+});
+
+app.get('/cr/images/:reviewId', (req, res) => {
+    proxy.web(req, res, {
+        target: customerReview
+      })
 });
 
 console.log("listening on port 5050");
-server.listen(5050);
+app.listen(5050);
