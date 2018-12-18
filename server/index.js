14e3a3c55d5236747c2fe95415b5eec65f13c236
DavidRez <davidraviles@gmail.com>
2018-12-18 00:42:56 -0800

    Group other components' server setup to bottom.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 let app = express();
 const port = 2000;
-const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require(__dirname + '/../knexfile.js')[environment];
@@ -86,5 +85,59 @@ app.get('/cr/products', (req, res) => {
         res.status(500).json({ error });
     });
 });
+
+
+
+////////PRODUCT VIEW
+app.get('/productsdisplay', (req, res) => {
+    let productID = req.query.id; // http://localhost:710/productsdisplay?id=91 gets product id 91
+    sequelize.query(`SELECT * FROM products WHERE id=${productID};`, {
+        type: sequelize.QueryTypes.SELECT
+      })
+      .then(data => {
+        res.status(200).json(data);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(error);
+      });
+  });
+  
+  app.get('/products', (req, res) => {
+    sequelize2.authenticate()
+      .then(() => {
+        console.log('Connection has been established successfully.');
+      })
+      .catch(err => {
+        console.error('Unable to connect to the database: ', err);
+        res.status(500).send();
+      });
+    sequelize2.query('SELECT * FROM products ORDER BY id', {
+        type: sequelize.QueryTypes.SELECT
+      })
+      .then(data => {
+        res.status(200).send(JSON.stringify(data));
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  });
+
+
+////product view and recommended products
+const Sequelize = require('sequelize');
+
+/////product view
+const dbUrl = process.env.DB_URL;
+const db2Url = process.env.RECOMMENDED_PRODUCTS_URL;
+
+const sequelize = new Sequelize(dbUrl, {
+  dialect: 'postgres'
+});
+
+const sequelize2 = new Sequelize(db2Url, {
+  dialect: 'postgres'
+});
+////////////
 
 module.exports = app;
